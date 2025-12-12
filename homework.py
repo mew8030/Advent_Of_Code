07da63f6e58a4dfc0ffa59d3ffab552f3d0c1f62
mew8030 <mew8030@proton.me>
2025-12-12 16:36:24 -0500

    B7: finished part two of the day 6 challenge going to day 7

--- homework.py
+++ homework.py
@@ -10,20 +10,42 @@ class Homework:
 
     def analyze_hw(self):
         tmp = []
+        zipped = []
         operators = {'+', '-', '*', '/'}
         try:
             with open(self.__file_path) as f:
                 for data_set in f.read().splitlines():
                     self.l.log_debug(1, f"reading data_set {data_set}")
-                    for data in data_set.split(' '):
-                        if data.isnumeric():
-                            tmp.append(int(data))
-                        elif data in operators:
-                            tmp.append(data)
-                        else:
-                            continue
-                    self.__hw.append(tmp)
-                    tmp = []
+                    
+                    self.__hw.append(data_set)
+
+                self.l.log_event(f"zipping the list")
+                zipped = list(zip(*self.__hw[:-1]))
+                self.l.log_event(f"extracting the operators")
+                ops = list("".join(self.__hw[-1:]))
+                self.l.log_event(f"clearing the homework analysis to make room for cephlapod method")
+                for i in range(0, len(self.__hw)):
+                    self.__hw[i] = []
+                self.l.log_event(f"clearing unneeded spaces on list for operators")
+                while ' ' in ops:
+                    ops.remove(' ')
+                self.l.log_debug(0, f"getting the list of operators {ops}")
+                self.l.log_debug(0, f"the homework analysis should be cleared self.__hw {self.__hw}")
+                self.l.log_event(f"setting up values for analysis after being zipped")
+                i = 0
+                for z in zipped:
+                    z = "".join(z).replace(' ', '')
+                    self.l.log_debug(0, f"getting data from zipped list {z}")
+                    if z != '':
+                        self.__hw[i].append(int(z))
+                        i += 1
+                    elif z == '':
+                        i = self.insert_empty(i)
+                while i != 0:
+                    i = self.insert_empty(i)
+                self.__hw[len(self.__hw) - 1] = ops
+
+            self.l.log_debug(1, f"Setup complete, displaying analysis")
             for line in self.__hw:
                 self.l.log_debug(1, f"{line}")
             self.l.log_event(f"homework analyzation complete")
@@ -32,12 +54,22 @@ class Homework:
         except ValueError as e:
             self.l.log_event(f"ERROR: {e}")
 
+
+    def insert_empty(self, i):
+        while i < len(self.__hw) - 1:
+            self.__hw[i].append([])
+            i += 1
+        return 0
+
+
     def setup_questions_and_answers(self):
         answer = 0
         for i in range(0, len(self.__hw[0])):
             operator = self.__hw[len(self.__hw) - 1][i]
             self.l.log_debug(0, f"the operator is {operator}")
             for j in range(0, len(self.__hw) - 1):
+                if self.__hw[j][i] == []:
+                    continue
                 if operator == '+':
                     self.l.log_debug(0, f"adding {self.__hw[j][i]} to {answer}")
                     answer += self.__hw[j][i]
